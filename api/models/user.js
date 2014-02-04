8aa98971b5c68b246264ee159b7e4ae1db6ce7d9
mimouncadosch <mimouncadosch@gmail.com>
2014-02-03 21:14:51 -0500

    setting up seed

---
+++ api/models/user.js
@@ -0,0 +1,63 @@
+// ./api/models/user.js
+
+// load the modules we need	
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+// define the schema for our user model
+var userSchema =  new Schema({
+	// GLOBALS
+	name	: String,
+	twitter_handle	: String
+	
+	
+	// NEEDED FOR PASSPORT AUTHORIZATION: http://passportjs.org/guide/profile/
+	provider: String,
+	displayName: String,
+	name: {
+		familyName: String,
+		givenName: String, 
+		middleName: String
+	},
+	emails: [{value: String, type: String}]
+});
+
+
+// methods =============================
+// check if password is valid using bcrypt
+userSchema.methods.validPassword = function(password) {
+	return bcrypt.compareSync(password, this.local.password);
+};
+
+// this method hashes the password and sets the user's password
+userSchema.methods.hashPassword = function(password) {
+	var user = this;
+
+	// hash the password
+	bcrypt.hash(password, null, null, function(err, hash) {
+		if (err)
+			return next(err);
+
+		user.local.password = hash;
+	});
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
+
+
+
+
+
+
+
+// create a user model
+var User = mongoose.model('User', {
+  oauthID: Number,
+  name: String,
+  created: Date
+});
+
+
+module.exports = User;
---
+++ config/database.js
@@ -0,0 +1,5 @@
+/* hold our database connection settings */ 
+module.exports = {
+	//========= Use MongoLab for DB =========
+    // 'url' : 'mongodb://crimeshark:columbia18@ds027769.mongolab.com:27769/crimeshark' // MongoHQ DB
+};
