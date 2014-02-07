bccfd811952333a0417e05eb8c792dc1a560aebe
mimouncadosch <mimouncadosch@gmail.com>
2014-02-07 16:52:27 -0500

    json data need to be asynchronous

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -29,10 +29,9 @@ angular.module('myApp.controllers', []).
 			$http({
 				method: 'GET',
 				url: '/api/cronJob?username=freeslugs',
-
 			}).success(function (data, status, headers, config) {
-				//$scope.tweets = data;
-				console.log(data);
+				draw(data);
+				// console.log(data);
 				//console.log($scope.tweets);
 			}).error(function (data, status, headers, config) {
 				console.log(data);
@@ -43,99 +42,86 @@ angular.module('myApp.controllers', []).
 		
 			console.log("program is calling to draw the map");
 			console.log("Date from DB");
+			console.log(data);
 			
-			var parsedDates = [];
-			for (var i = 0; i < data.length; i++) {
+			// var parsedDates = [];
+			// for (var i = 0; i < data.length; i++) {
 
-				var db_date = data[i].created_at;
-				console.log("db_date: ");
-				console.log(db_date);
+			// 	var db_date = data[i].created_at;
+			// 	console.log("db_date: ");
+			// 	console.log(db_date);
 
-				var JS_date = new Date(db_date);
-				console.log("JS_date");
-				console.log(JS_date);
+			// 	var JS_date = new Date(db_date);
+			// 	console.log("JS_date");
+			// 	console.log(JS_date);
 				
-				parsedDates.push(JS_date);
-				
-			};
-
-			console.log("length: ");
-			console.log(parsedDates.length);
-
-			console.log("parsedDates");
-
-			console.log(parsedDates[3]);
-			console.log(parsedDates[4]);
-
-
-			// create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
-			// var data = [3, 6, 2, 7, 5, 2, 0, 3, 8, 9, 12, 5, 9, 3, 6, 3, 6, 2, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7];
-
-
-			// write Ctrl here
-			var m = [80, 200, 80, 200]; // margins
-			var w = 1000 - m[1] - m[3]; // width
-			var h = 400 - m[0] - m[2]; // height
-
-		// X scale will fit all values from data[] within pixels 0-w
-		// var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
-		var x = d3.time.scale()
-		.domain([new Date(parsedDates[0]), d3.time.day.offset(new Date(parsedDates[parsedDates.length - 1]), 1)])
-		// .rangeRound([0, w - m.left - m.right]);
-
-		// Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
-		var y = d3.scale.linear().domain([0, 20]).range([h, 0]);
-			// automatically determining max range can work something like this
-			// var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
-
-		// create a line function that can convert data[] into x and y points
-		var line = d3.svg.line()
-			// assign the X function to plot our line as we wish
-			.x(function(d,i) { 
-				// verbose logging to show what's actually being done
-				console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
-				// return the X coordinate where we want to plot this datapoint
-				return x(i); 
-			})
-			.y(function(d) { 
-				// verbose logging to show what's actually being done
-				console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
-				// return the Y coordinate where we want to plot this datapoint
-				return y(d); 
-			})
-
-			// Add an SVG element with the desired dimensions and margin.
-			var graph = d3.select("#graph").append("svg:svg")
-			.attr("width", w + m[1] + m[3])
-			.attr("height", h + m[0] + m[2])
-			.append("svg:g")
-			.attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-
-			// create yAxis
-			var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-			// Add the x-axis.
-			graph.append("svg:g")
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + h + ")")
-			.call(xAxis);
-
-
-			// create left yAxis
-			var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
-			// Add the y-axis to the left
-			graph.append("svg:g")
-			.attr("class", "y axis")
-			.attr("transform", "translate(-25,0)")
-			.call(yAxisLeft);
+			// 	parsedDates.push(JS_date);
+			// }
 			
-  			// Add the line by appending an svg:path element with the data line we created above
-			// do this AFTER the axes above so that the line is above the tick-lines
-			graph.append("svg:path").attr("d", line(data.created_at));
+
+			var margin = {top: 20, right: 20, bottom: 30, left: 50},
+			    width = 960 - margin.left - margin.right,
+			    height = 500 - margin.top - margin.bottom;
+
+			var parseDate = d3.time.format("%d-%b-%y").parse;
+
+			var x = d3.time.scale()
+			    .range([0, width]);
+
+			var y = d3.scale.linear()
+			    .range([height, 0]);
+
+			var xAxis = d3.svg.axis()
+			    .scale(x)
+			    .orient("bottom");
+
+			var yAxis = d3.svg.axis()
+			    .scale(y)
+			    .orient("left");
+
+			var line = d3.svg.line()
+			    .x(function(d) { return x(d.date); })
+			    .y(function(d) { return y(d.close); });
+
+			var svg = d3.select("body").append("svg")
+			    .attr("width", width + margin.left + margin.right)
+			    .attr("height", height + margin.top + margin.bottom)
+			  .append("g")
+			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
+			d3.json('/api/cronJob?	username=freeslugs', function(error, data) {
+				console.log("HERE IS DATA IN D3");
+				console.log(data);
+			  data.forEach(function(d) {
+			    d.created_at = parseDate(d.created_at);
+			    d.sentiment = d.sentiment;
+			  });
+
+			  x.domain(d3.extent(data, function(d) { return d.date; }));
+			  y.domain(d3.extent(data, function(d) { return d.close; }));
+
+			  svg.append("g")
+			      .attr("class", "x axis")
+			      .attr("transform", "translate(0," + height + ")")
+			      .call(xAxis);
+
+			  svg.append("g")
+			      .attr("class", "y axis")
+			      .call(yAxis)
+			    .append("text")
+			      .attr("transform", "rotate(-90)")
+			      .attr("y", 6)
+			      .attr("dy", ".71em")
+			      .style("text-anchor", "end")
+			      .text("Sentiment");
+
+			  svg.append("path")
+			      .datum(data)
+			      .attr("class", "line")
+			      .attr("d", line);
+			});
 		}
-	
-
 
 	}).
 
