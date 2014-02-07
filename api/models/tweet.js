8bf94d2296c56fab36bfa1e184689fed83ef0087
mimouncadosch <mimouncadosch@gmail.com>
2014-02-06 23:11:19 -0500

    tweet model

---
+++ api/models/tweet.js
@@ -0,0 +1,23 @@
+// ./api/models/user.js
+
+// load the modules we need	
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// define the schema for our user model
+var tweetSchema =  new Schema({
+	// GLOBALS
+	user         	 					: {
+        name           					: String,
+        screen_name     				: String,
+        location        				: String,
+        url         					: String,
+        followers_count					: Number,
+        profile_background_image_url	: String
+    },
+    text: String,
+	created_at: Date,
+	sentiment: Number
+});
+
+module.exports = mongoose.model('Tweet', tweetSchema);
